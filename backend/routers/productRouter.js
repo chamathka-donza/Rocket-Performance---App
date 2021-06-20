2f56acf1f140bfca67750608e25731f364d4d05e
ChamathkaDonza\Chamathka <yapacs_im17094@stu.kln.ac.lk>
2021-06-20 13:19:20 -0700

    Admin Panel

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import Product from '../models/productModel.js';
+import { generateToken } from '../utils.js';
 
 const productRouter = express.Router();
 
@@ -29,5 +30,34 @@ productRouter.get('/:id', expressAsyncHandler(async(req, res) =>{
         res.status(404).send({ message: 'Product Not Found'});
     }
 }));
+
+productRouter.post('/addProduct', expressAsyncHandler(async(req, res) =>{
+    const product = Product({
+        c_id: req.body.c_id,
+        catogory: req.body.catogory,
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        description: req.body.description,
+        countInStock: req.body.countInStock
+
+    });
+    const createdProduct = await product.save();
+    res.send({
+
+        _id: createdProduct._id,
+        c_id: createdProduct.c_id,
+        catogory: createdProduct.catogory,
+        name: createdProduct.name,
+        image: createdProduct.image,
+        price: createdProduct.price,
+        description: createdProduct.description,
+        countInStock: createdProduct.countInStock,
+        token: generateToken(createdProduct),
+    })
+}
+
+
+))
 
 export default productRouter;
